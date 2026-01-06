93f5711ce7b655493588925a199834b78ebdc432
anjulanadeeshan <mranjula660@gmail.com>
2026-01-07 00:46:39 +0530

    crud operations done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,29 @@
         </form>
 
         <div style="border: 3px solid black">
+            <h2>Create a new post</h2>
             <form action="/createpost" method="POST">
             @csrf
             <input type="text" name="title" placeholder="post-title">
             <textarea name="body" placeholder="Body content..."></textarea>
             <button>Save post</button>
             </form>
+        </div>
+
+        <div style="border: 3px solid black">
+            <h2>All posts</h2>
+            @foreach($posts as $post)
+            <div style="background-color: gray; padding:10px; margin:10px;">
+                <h3>{{$post['title']}}</h3>
+                {{$post['body']}}
+                <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+                <form action="/delete-post/{{$post->id}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
+            </div>
+            @endforeach
         </div>
     @else
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('home');
+    $posts =[];
+    if(auth()->check()) {
+    $posts = auth()->user()->usersCoolPosts()->latest()->get();
+
+    }
+    // $posts = Post::where('user_id', auth()->id())->get();
+    return view('home', ['posts' => $posts]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
@@ -17,3 +24,8 @@ Route::post('/login', [UserController::class, 'login']);
 // Blog post related routes
 
 Route::post('/createpost',[PostController::class, 'createPost']);
+
+Route::get('edit-post/{post}', [PostController::class, 'showEditScreen']);
+
+Route::put('edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('delete-post/{post}', [PostController::class, 'deletePost']);
